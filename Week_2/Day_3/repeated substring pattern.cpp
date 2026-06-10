64671673c4981e7d04afdfafc32d52422915b000
PoojaSharma182 <24112cn117@glbitm.ac.in>
2026-06-10 13:50:20 +0530

    Added find the index of first occurance in a string, is subsequence, repeated substring pattern

---
+++ Week_2/Day_3/repeated substring pattern.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        string t=s+s;
+        t=t.substr(1,t.size()-2);
+        return t.find(s)!=string::npos;
+    
+    }
+};
