e032cd83bdc72bf8a6062a695cc61ca84d2f0bd6
PoojaSharma182 <24112cn117@glbitm.ac.in>
2026-06-11 10:48:08 +0530

    Added reverse words in a string III,Decode string,Generate parentheses

---
+++ Week_2/Day_4/Reverse words in a string III.cpp
@@ -0,0 +1,35 @@
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        int start=0;
+        
+        for(int i=0;i<n;i++){
+            if(s[i]==' '){
+                int left=start;
+                int right=i-1;
+            while(left<right){
+                swap(s[left],s[right]);
+                left++;
+                right--;
+
+            }
+            start=i+1;
+            }
+        }
+        int left=start;
+        int right=n-1;
+         while(left<right){
+                swap(s[left],s[right]);
+                left++;
+                right--;
+
+            }
+
+        return s;
+    }
+};
