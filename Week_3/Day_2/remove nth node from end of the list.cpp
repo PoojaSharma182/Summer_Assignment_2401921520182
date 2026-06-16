4efebaf8ad77e0f22896d9b104753794cf7b265c
PoojaSharma182 <24112cn117@glbitm.ac.in>
2026-06-16 11:09:04 +0530

    Added merge two sorted list, remove nth node from end of list,Palindrome linked list

---
+++ Week_3/Day_2/remove nth node from end of the list.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode dummy(0);
+        dummy.next=head;
+        ListNode* fast=&dummy;
+        ListNode* slow=&dummy;
+        for(int i=0;i<=n;i++){
+            fast=fast->next;
+        }while(fast){
+            fast=fast->next;
+            slow=slow->next;
+        }
+        ListNode*del=slow->next;
+        slow->next=del->next;
+        delete del;
+        return dummy.next;
+    }
+
+};
